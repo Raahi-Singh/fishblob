d6d992c3e9a9030223612603d1c867a27a5b7b1c
Raahi <raahi.singh@hotmail.com>
2023-05-31 19:28:28 +1000

    stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Index() {
   return (
     <div className="p-10 prose">
-      <h1>Fish Blob</h1>
+      <h1 className="text-4xl font-extrabold">Fish Blob</h1>
+      <br/>
       <Image src="/fish-blob.png" alt="Fish Blob" width={500} height={500} />
     </div>
   );
