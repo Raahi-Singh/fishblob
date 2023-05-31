71d32f0eed256d8b8f549fddec055e4ccc3c6eb1
Yuze Ding <67092737+Yzding26@users.noreply.github.com>
2023-05-31 20:12:07 +1000

    returning title to normal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Index() {
   return (
     <div className="p-10 prose">
-      <h1 className="text-4xl font-extrabold">FishblobFishBlobFish Blob?</h1>
+      <h1 className="text-4xl font-extrabold">The fishblob site</h1>
       <br/>
       <Image src="/fish-blob.png" alt="Fish Blob" width={500} height={500} />
       <h1 className="text-2x1 font-italics">This is a Fish Blob</h1>
