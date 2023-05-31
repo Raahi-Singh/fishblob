815f92e1b5b9a532a1eda6456c20d959b273c869
Yuze Ding <67092737+Yzding26@users.noreply.github.com>
2023-05-31 19:44:41 +1000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Index() {
   return (
     <div className="p-10 prose">
-      <h1 className="text-4xl font-extrabold">FishblobFish</h1>
+      <h1 className="text-4xl font-extrabold">FishblobFishBlob</h1>
       <br/>
       <Image src="/fish-blob.png" alt="Fish Blob" width={500} height={500} />
     </div>
