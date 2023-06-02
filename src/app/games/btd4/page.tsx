108550d25aae923219c3c9fabf4c4472054ee475
Raahi <raahi.singh@hotmail.com>
2023-06-02 19:49:27 +1000

    Navbar works on mobile kinda

--- src/app/games/btd4/page.tsx
+++ src/app/games/btd4/page.tsx
@@ -2,7 +2,7 @@
 export default function Index() {
   return (
     <div className="p-10 prose">
-      <h1 className="text-4xl font-extrabold">Snake</h1>
+      <h1 className="text-4xl font-extrabold">BTD 4</h1>
       <br />
       <iframe className="w-screen h-[500px]" src="https://www.addictinggames.com/embed/html5-games/20543" scrolling="no"></iframe>
     </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,13 +10,19 @@ export const metadata = {
   description: 'Website Feauturing FishBlob, Spider-Man, and Rulers',
 }
 
+function MobileNav() {
+    alert("Mobile Nav")
+}
+
+
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <>
       <body className={inter.className}>
       <header aria-label="Site Header" className="bg-white">
   <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
@@ -29,7 +35,7 @@ export default function RootLayout({
       </div>
 
       <div className="md:flex md:items-center md:gap-12">
-        <nav aria-label="Site Nav" className="hidden md:block">
+        <nav aria-label="Site Nav">
           <ul className="flex items-center gap-6 text-sm">
             <li>
               <Link
@@ -66,73 +72,18 @@ export default function RootLayout({
                 Fishblobs
               </Link>
             </li>
-
-            <li>
-              <Link
-                className="text-gray-500 transition hover:text-gray-500/75"
-                href="/"
-              >
-                Projects
-              </Link>
-            </li>
-
-            <li>
-              <Link
-                className="text-gray-500 transition hover:text-gray-500/75"
-                href="/"
-              >
-                Blog
-              </Link>
-            </li>
-          </ul>
+            </ul>
         </nav>
 
-        <div className="flex items-center gap-4">
-          <div className="sm:flex sm:gap-4">
-            <Link
-              className="rounded-md bg-sky-700 px-5 py-2.5 text-sm font-medium text-white shadow"
-              href="/"
-            >
-              Login
-            </Link>
+        
 
-            <div className="hidden sm:flex">
-              <Link
-                className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-sky-700"
-                href="/"
-              >
-                Register
-              </Link>
-            </div>
           </div>
-
-          <div className="block md:hidden">
-            <button
-              className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            </button>
           </div>
         </div>
-      </div>
-    </div>
-  </div>
-</header>
+      </header>
+
         {children}
         </body>
-    </html>
+    </>
   )
 }
