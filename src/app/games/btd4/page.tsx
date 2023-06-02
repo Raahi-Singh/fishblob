705b732d1b1bf3bf4cc0acaaf722a5cde9420f4e
Raahi <raahi.singh@hotmail.com>
2023-06-02 18:00:24 +1000

    BTD4 + Fixed Games Page

---
+++ src/app/games/btd4/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Index() {
+  return (
+    <div className="p-10 prose">
+      <h1 className="text-4xl font-extrabold">Snake</h1>
+      <br />
+      <iframe className="w-screen h-[500px]" src="https://www.addictinggames.com/embed/html5-games/20543" scrolling="no"></iframe>
+    </div>
+  );
+}
--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -3,20 +3,21 @@ import Link from "next/link";
 type GameProps = {
     name: string;
     desc: string;
+    page: string;
+    bg: string;
   };
   
-  function Game({ name, desc }: GameProps) {
+  function Game({ name, desc, page, bg }: GameProps) {
   return (
     <Link
-      href="#"
-      className=" mt-5 relative block overflow-hidden rounded-xl bg-[url(https://images.unsplash.com/photo-1552832230-c0197dd311b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1592&q=80)] bg-cover bg-center bg-no-repeat"
+      href={page}
+      className={"mt-5 relative block overflow-hidden rounded-xl bg-cover bg-center bg-no-repeat " + bg}
     >
       <div className="absolute inset-0 bg-black/25"></div>
 
       <div className="relative flex items-start justify-between p-4 sm:p-6 lg:p-8">
         <div className="sm:pt-18 pt-12 text-white lg:pt-24">
           <h3 className="text-xl font-bold sm:text-2xl">{name}</h3>
-
           <p className="text-sm">{desc}</p>
         </div>
       </div>
@@ -34,7 +35,8 @@ export default function Index() {
         <br />
         <h1 className="text-4xl font-extrabold">Games: </h1>
         <br />
-        <Game name="Slope" desc="A game about a ball on a slope"   />
+        <Game name="Slope" desc="A game about a ball on a slope"  page="/games/slope" bg="bg-gradient-to-r from-green-500 to-red-500" />
+        <Game name="BTD 4" desc="Bloons Tower Defense 4" page="/games/btd4" bg="bg-gradient-to-r from-blue-500 to-purple-500" />
       </section>
     </>
   );
