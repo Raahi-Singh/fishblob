62d881d22ab889a738bcf455ca03c1b2ab55325f
Raahi-Singh <125248982+Raahi-Singh@users.noreply.github.com>
2023-06-06 10:25:44 +1000

    Update page.tsx

--- src/app/entertainment/slope/page.tsx
+++ src/app/entertainment/slope/page.tsx
@@ -6,7 +6,7 @@ export default function Index() {
     <div className="p-10 prose">
       <h1 className="text-4xl font-extrabold">Slope</h1>
       <br/>
-      <iframe src="https://slope.abnumality.repl.co/"></iframe>
-    </div>
+      
+slope    </div>
   );
 }
