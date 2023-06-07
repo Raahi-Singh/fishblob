74dff1de508b76576c5f714e39549f507aecce1f
Yuze Ding <67092737+Yzding26@users.noreply.github.com>
2023-06-07 21:19:57 +1000

    update landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Index() {
   return (
     <>
       <div className="bg-gray-100">
-        <header className="bg-pink-200 shadow" style={{ height: '10vh' }}>
+        <header className="bg-pink-200 shadow" style={{ height: '15vh' }}>
           <div className="container mx-auto px-4 py-6">
             <h1 className="text-xl sm:text-xl md:text-2xl lg:text-3xl font-semibold text-white truncate">Welcome to The Fishblob Site</h1>
             <p className="text-base sm:text-xl md:text-xl lg:text-2xl font-semibold text-white truncate">The best website on the whole internet for your spiderman, fishblob, and ruler needs.</p>
@@ -13,14 +13,14 @@ export default function Index() {
         </header>
 
         <main className="bg-gray-100">
-          <section className="bg-blue-200 bg-cover bg-center py-32 sm:py-48" style={{ backgroundImage: 'url(https://upload.wikimedia.org/wikipedia/commons/9/99/Blobfish-vector.svg)', height: '43vh', backgroundSize: 'contain', backgroundRepeat: 'no-repeat' }}>
+          <section className="bg-blue-200 bg-cover bg-center py-32 sm:py-48" style={{ backgroundImage: 'url(https://upload.wikimedia.org/wikipedia/commons/9/99/Blobfish-vector.svg)', height: '43vh', backgroundSize: '35%', backgroundRepeat: 'no-repeat' }}>
             <div className="container mx-auto text-center">
               <h2 className="text-3xl font-semibold text-white">Explore</h2>
               <p className="text-white">Countless random pages and fun ideas</p>
             </div>
           </section>
 
-          <section className="bg-green-200 bg-cover bg-center py-32 sm:py-48" style={{ backgroundImage: 'url(https://i.pinimg.com/originals/50/6a/76/506a762d35d035d05b555b50e488c6b2.png)', height: '43vh', backgroundSize: 'contain', backgroundRepeat: 'no-repeat' }}>
+          <section className="bg-red-200 bg-cover bg-center py-32 sm:py-48" style={{ backgroundImage: 'url(https://i.pinimg.com/originals/50/6a/76/506a762d35d035d05b555b50e488c6b2.png)', height: '43vh', backgroundSize: '20%', backgroundRepeat: 'no-repeat' }}>
             <div className="container mx-auto text-center">
               <h2 className="text-3xl font-semibold text-white">Games</h2>
               <p className="text-white">We currently have slope, bee swarm simulator, and block smasher</p>
