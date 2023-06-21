d29cf2ac4fbfe2c86d950505ff456adbc3d865c5
Raahi-Singh <125248982+Raahi-Singh@users.noreply.github.com>
2023-06-22 09:29:08 +1000

    date fixed

--- src/app/spider/page.tsx
+++ src/app/spider/page.tsx
@@ -6,7 +6,7 @@ import React, { useState, useEffect } from 'react';
 export default function Spider() {
     const [days, setDays] = useState(null)
     useEffect(() => {
-        today = new Date();
+        let today = new Date();
         var btsv = new Date(today.getFullYear(), 3, 29);
         if (today.getMonth() == 11 && today.getDate() > 25) {
             btsv.setFullYear(cmas.getFullYear() + 1);
