7f43d979443d38e1d3c298f9e74cca7eb4eca576
Raahi-Singh <125248982+Raahi-Singh@users.noreply.github.com>
2023-06-22 09:27:55 +1000

    date til btsv fixed

--- src/app/spider/page.tsx
+++ src/app/spider/page.tsx
@@ -3,18 +3,21 @@ import { Varta } from 'next/font/google';
 import React, { useState, useEffect } from 'react';
 
 
-export default function Spider(){
+export default function Spider() {
     const [days, setDays] = useState(null)
     useEffect(() => {
-        var date: any = new Date();
-        date = date.getDate();
-        console.log(date);
-        setDays(date);
+        today = new Date();
+        var btsv = new Date(today.getFullYear(), 3, 29);
+        if (today.getMonth() == 11 && today.getDate() > 25) {
+            btsv.setFullYear(cmas.getFullYear() + 1);
+        }
+        var one_day = 1000 * 60 * 60 * 24;
+        let days = Math.ceil((cmas.getTime() - today.getTime()) / (one_day))
     });
 
     return (
         <>
-        There are {days} until BTSV
+            There are {days} until BTSV
         </>
     )
 }
