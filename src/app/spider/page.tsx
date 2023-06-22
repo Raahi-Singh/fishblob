9ff71a2ab9d002be3c9c25159a4c216d48e73363
Raahi-Singh <125248982+Raahi-Singh@users.noreply.github.com>
2023-06-22 10:32:47 +1000

    Update page.tsx

--- src/app/spider/page.tsx
+++ src/app/spider/page.tsx
@@ -3,15 +3,13 @@ import React, { useState, useEffect } from 'react';
 
 
 export default function Spider() {
-    useEffect(() => {
-        let today = new Date();
+    let today = new Date();
         var btsv = new Date(today.getFullYear(), 3, 29);
         if (today.getMonth() == 11 && today.getDate() > 25) {
             btsv.setFullYear(btsv.getFullYear() + 1);
         }
         var one_day = 1000 * 60 * 60 * 24;
         let days = Math.ceil((btsv.getTime() - today.getTime()) / (one_day))
-    });
 
     return (
         <>
